6ec61d4b1b3e1ba69ab2cf04fda0d15e307ef7ba
Alexander Markevich <alexmarkevich1996@gmail.com>
2026-08-11 12:46:42 +0300

    Edit models: CertificateSetting and Course

---
+++ EntityFramework_lesson/Migrations/20260811094552_EdittedCertificateSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFramework_lesson.Migrations
+{
+    /// <inheritdoc />
+    public partial class EdittedCertificateSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- TestDB/Models/Course.cs
+++ TestDB/Models/Course.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace stepik.Models
 {
+    [Table("courses")]
     public class Course
     {
+        [Column("id")]
         public int Id { get; set; }
-        public string Title { get; set; } = default!;
+        [Column("title")]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Column("summary")]
         public string? Summary { get; set; }
+        [Column("photo")]
         public string? Photo { get; set; }
+        [Column("price")]
+        public decimal Price { get; set; }
+        public CertificateSetting CertificateSetting { get; set; }
     }
 }
